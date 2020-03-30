99cf4a05e672f552193bd89741635715c2838acf
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-03-30 19:06:17 +0200

    Added test for L1 norm

--- examples/nvector/psblas/test_nvector_psblas.c
+++ examples/nvector/psblas/test_nvector_psblas.c
@@ -173,7 +173,7 @@ int main(int argc, char *argv[])
   fails += Test_N_VWrmsNormMask(X, Y, Z, local_length, global_length, myid);
   fails += Test_N_VMin(X, local_length, myid);
   fails += Test_N_VWL2Norm(X, Y, local_length, global_length, myid);
-  // fails += Test_N_VL1Norm(X, local_length, global_length, myid);
+  fails += Test_N_VL1Norm(X, local_length, global_length, myid);
   // fails += Test_N_VCompare(X, Z, local_length, myid);
   fails += Test_N_VInvTest(X, Z, local_length, myid);
   fails += Test_N_VConstrMask(X, Y, Z, local_length, myid);
