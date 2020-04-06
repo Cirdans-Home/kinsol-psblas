ee0b4640c8af32286fb3f7ad36599b20b162dc32
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-04-06 12:51:45 +0200

    Added implementation of nnz function for distributed sparse matrix

--- src/sunmatrix/psblas/sunmatrix_psblas.c
+++ src/sunmatrix/psblas/sunmatrix_psblas.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 
 #include <sunmatrix/sunmatrix_psblas.h>
+#include <nvector/nvector_psblas.h>
 #include <sundials/sundials_nvector.h>
 #include <sundials/sundials_math.h>
 
@@ -50,6 +51,51 @@ SUNMatrix SUNPSBLASMatrix(int ictxt, psb_c_descriptor *cdh)
   SUNMatrix A;
   SUNMatrix_Ops ops;
   SUNMatrixContent_PSBLAS content;
+  psb_c_dspmat *ah;
+  psb_i_t ret;
+
+  /* Create Matrix */
+  A = NULL;
+  A = (SUNMatrix) malloc(sizeof *A);
+  if (A == NULL) return(NULL);
+
+  /* Create matrix operation structure */
+  ops = NULL;
+  ops = (SUNMatrix_Ops) malloc(sizeof(struct _generic_SUNMatrix_Ops));
+  if (ops == NULL) { free(A); return(NULL); }
+
+  /* Attach operations */
+  ops->getid       = SUNMatGetID_PSBLAS;
+  ops->clone       = SUNMatClone_PSBLAS;
+  ops->destroy     = SUNMatDestroy_PSBLAS;
+  ops->zero        = SUNMatZero_PSBLAS;
+  ops->copy        = SUNMatCopy_PSBLAS;
+  ops->scaleadd    = SUNMatScaleAdd_PSBLAS;
+  ops->scaleaddi   = SUNMatScaleAddI_PSBLAS;
+  ops->matvec      = SUNMatMatvec_PSBLAS;
+  ops->space       = SUNMatSpace_PSBLAS;
+
+  /* Create content */
+  content = NULL;
+  content = (SUNMatrixContent_PSBLAS) malloc(sizeof(struct _SUNMatrixContent_Sparse));
+  if (content == NULL) { free(ops); free(A); return(NULL); }
+
+  content->ictxt = ictxt;
+  content->cdh   = cdh;
+  content->sparsetype = PSBLAS_MAT;
+  ah  = psb_c_new_dspmat();
+  ret = psb_c_dspall(ah,cdh);
+  if(ret == 0){
+    free(content);
+    free(ops);
+    free(A);
+    return(NULL);
+  }
+  content->ah = ah;
+
+  /* Attach content and ops */
+  A->content = content;
+  A->ops     = ops;
 
   return(A);
 }
@@ -87,8 +133,10 @@ SUNMatrix SUNPSBLASFromBandMatrix(SUNMatrix Ad, realtype droptol, int sparsetype
  * Function to print the sparse matrix
  */
 
-void SUNPSBLASMatrix_Print(SUNMatrix A, FILE* outfile)
+void SUNPSBLASMatrix_Print(SUNMatrix A, char *matrixtitle, char* filename)
 {
+
+  psb_c_dmm_mat_write( SM_PMAT_P(A), matrixtitle, filename);
 
   return;
 }
@@ -100,32 +148,32 @@ void SUNPSBLASMatrix_Print(SUNMatrix A, FILE* outfile)
 
 sunindextype SUNPSBLASMatrix_Rows(SUNMatrix A)
 {
-  if (SUNMatGetID(A) == SUNMATRIX_SPARSE)
-    return 0;
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dmat_get_nrows(SM_PMAT_P(A)));
   else
     return -1;
 }
 
 sunindextype SUNPSBLASMatrix_Columns(SUNMatrix A)
 {
-  if (SUNMatGetID(A) == SUNMATRIX_SPARSE)
-    return 0;
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dmat_get_ncols(SM_PMAT_P(A)));
   else
     return -1;
 }
 
 sunindextype SUNPSBLASMatrix_NNZ(SUNMatrix A)
 {
-  if (SUNMatGetID(A) == SUNMATRIX_SPARSE)
-    return 0;
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dnnz(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)));
   else
     return -1;
 }
 
 int SUNPSBLASMatrix_PSBLASType(SUNMatrix A)
 {
-  if (SUNMatGetID(A) == SUNMATRIX_SPARSE)
-    return 0;
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return PSBLAS_MAT;
   else
     return -1;
 }
@@ -134,7 +182,7 @@ realtype* SUNPSBLASMatrix_Data(SUNMatrix A)
 {
   realtype dummy[10];
 
-  if (SUNMatGetID(A) == SUNMATRIX_SPARSE)
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
     return dummy;
   else
     return NULL;
@@ -154,12 +202,66 @@ SUNMatrix_ID SUNMatGetID_PSBLAS(SUNMatrix A)
 SUNMatrix SUNMatClone_PSBLAS(SUNMatrix A)
 {
   SUNMatrix B;
+  SUNMatrix_Ops ops;
+  SUNMatrixContent_PSBLAS content;
+  psb_c_dspmat *ah;
+  psb_i_t ret;
+
+  /* Create Matrix */
+  B = NULL;
+  B = (SUNMatrix) malloc(sizeof *A);
+  if (B == NULL) return(NULL);
+
+  /* Create matrix operation structure */
+  ops = NULL;
+  ops = (SUNMatrix_Ops) malloc(sizeof(struct _generic_SUNMatrix_Ops));
+  if (ops == NULL) { free(B); return(NULL); }
+
+  /* Attach operations */
+  ops->getid       = SUNMatGetID_PSBLAS;
+  ops->clone       = SUNMatClone_PSBLAS;
+  ops->destroy     = SUNMatDestroy_PSBLAS;
+  ops->zero        = SUNMatZero_PSBLAS;
+  ops->copy        = SUNMatCopy_PSBLAS;
+  ops->scaleadd    = SUNMatScaleAdd_PSBLAS;
+  ops->scaleaddi   = SUNMatScaleAddI_PSBLAS;
+  ops->matvec      = SUNMatMatvec_PSBLAS;
+  ops->space       = SUNMatSpace_PSBLAS;
+
+  /* Create content */
+  content = NULL;
+  content = (SUNMatrixContent_PSBLAS) malloc(sizeof(struct _SUNMatrixContent_Sparse));
+  if (content == NULL) { free(ops); free(A); return(NULL); }
+
+  content->ictxt = SM_ICTXT_P(A);
+  content->cdh   = SM_DESCRIPTOR_P(A);
+  content->sparsetype = PSBLAS_MAT;
+  ah  = psb_c_new_dspmat();
+  ret = psb_c_dspall(ah,SM_DESCRIPTOR_P(A));
+  if(ret == 0){
+    free(content);
+    free(ops);
+    free(B);
+    return(NULL);
+  }
+  content->ah = ah;
+
+  /* Attach content and ops */
+  B->content = content;
+  B->ops     = ops;
 
   return(B);
 }
 
 void SUNMatDestroy_PSBLAS(SUNMatrix A)
 {
+  /* Free the PSBLAS Sparse Matrix */
+  psb_c_dspfree(SM_PMAT_P(A), SM_DESCRIPTOR_P(A));
+  /* Free the NMATRIX_PSBLAS Structure */
+  free(A->content); A->content = NULL;
+  free(A->ops);  A->ops = NULL;
+  free(A); A = NULL;
+
   return;
 }
 
@@ -170,6 +272,8 @@ int SUNMatZero_PSBLAS(SUNMatrix A)
 
 int SUNMatCopy_PSBLAS(SUNMatrix A, SUNMatrix B)
 {
+
+
 
   return 0;
 }
@@ -189,6 +293,9 @@ int SUNMatScaleAdd_PSBLAS(realtype c, SUNMatrix A, SUNMatrix B)
 
 int SUNMatMatvec_PSBLAS(SUNMatrix A, N_Vector x, N_Vector y)
 {
+
+  psb_c_dspmm( (psb_d_t) 1.0, SM_PMAT_P(A), NV_PVEC_P(x) ,
+  		    (psb_d_t) 1.0, NV_PVEC_P(y) , NV_DESCRIPTOR_P(x));
 
   return 0;
 }
